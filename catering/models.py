5e330f4137528b57182f86f960d32abbea1239c1
Andrei Madiev <am@yourfirm.de>
2021-05-26 23:01:11 +0200

    Initialize catering app and create models

---
+++ catering/models.py
@@ -0,0 +1,121 @@
+import enum
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+@enum.unique
+class IngredientType(enum.Enum):
+    BEEF = 'Beef'
+    CHICKEN = 'Chicken'
+    PORK = 'Pork'
+    VEGETARIAN = 'Vegetarian'
+    VEGAN = 'Vegan'
+
+    @staticmethod
+    def get_choices():
+        return [(t.name, t.value) for t in IngredientType]
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RestaurantType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    type = models.CharField(max_length=10, choices=IngredientType.get_choices())
+    price_per_portion = models.PositiveIntegerField()
+    total_amount = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Portion(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200, default='')
+    ingredients = models.ManyToManyField(Ingredient)
+    is_beverage = models.BooleanField(default=False)
+
+    @property
+    def price(self):
+        return sum([ingredient.price_per_portion for ingredient in self.ingredients.all()]) * settings.MARGIN_MULTIPLIER
+
+    @property
+    def is_vegetarian(self):
+        return all([ingredient.type == IngredientType.VEGETARIAN for ingredient in self.ingredients.all()])
+
+    @property
+    def is_vegan(self):
+        return all([ingredient.type == IngredientType.VEGAN for ingredient in self.ingredients.all()])
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    type = models.ForeignKey(RestaurantType, on_delete=models.PROTECT)
+    serves_food = models.BooleanField(default=False)
+    serves_beverage = models.BooleanField(default=False)
+    menu = models.ManyToManyField(Portion)
+
+    def clean(self):
+        super().clean()
+
+        if not self.serves_beverage and self.menu.filter(is_beverage=True).exists():
+            raise ValidationError('Restaurant does not serve beverages')
+
+        if not self.serves_food and self.menu.filter(is_beverage=False).exists():
+            raise ValidationError('Restaurant does not serve any food')
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.clean()
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return f'{self.name} in {self.city}'
+
+
+class Guest(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    address = models.CharField(max_length=100)
+    visit_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user.get_full_name()} visited on {self.visit_time}'
+
+
+class Order(models.Model):
+    guest = models.ForeignKey(User, on_delete=models.PROTECT)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
+
+    @property
+    def total_price(self):
+        return sum([portion.price for portion in self.ordered_portions.all()])
+
+    def __str__(self):
+        return f'Order for {self.guest.get_full_name()} in {self.restaurant} for {self.total_price}'
+
+
+class OrderedPortion(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='ordered_portions')
+    portion = models.ForeignKey(Portion, on_delete=models.CASCADE)
+    amount = models.PositiveSmallIntegerField()
+
+    @property
+    def price(self):
+        return self.portion.price * self.amount
