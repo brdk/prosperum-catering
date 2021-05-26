636d1748b4ae73aa8cb78b7539804d4205e62a88
Andrei Madiev <am@yourfirm.de>
2021-05-27 00:29:17 +0200

    Register admin views

--- catering/admin.py
+++ catering/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from catering.models import City, RestaurantType, Ingredient, Portion, Restaurant, Guest, Order, OrderedPortion
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'price_per_portion', 'total_amount']
+
+
+class PortionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_beverage', 'get_ingredients', 'price']
+
+    def get_ingredients(self, obj):
+        return ', '.join(obj.ingredients.all().values_list('name', flat=True))
+    get_ingredients.short_description = 'ingredients'
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'city', 'type', 'serves_food', 'serves_beverage', 'get_menu']
+
+    def get_menu(self, obj):
+        return ', '.join(obj.menu.all().values_list('name', flat=True))
+    get_menu.short_description = 'menu'
+
+
+admin.site.register(City)
+admin.site.register(RestaurantType)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Portion, PortionAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Guest)
+admin.site.register(Order)
+admin.site.register(OrderedPortion)
