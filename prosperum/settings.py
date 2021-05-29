dbebf7902a9d8abd35eda23761a020e8125fbac1
Andrei Madiev <am@yourfirm.de>
2021-05-29 22:02:48 +0200

    Install and configure django filter

--- prosperum/settings.py
+++ prosperum/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'djoser',
     'catering',
 ]
@@ -133,6 +134,9 @@ MARGIN_MULTIPLIER = 3
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
 
